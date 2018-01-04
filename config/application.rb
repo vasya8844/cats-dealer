061d3d1bd9d2fd9616f65ca683fa69eeddfbcbda
Vasyl Boychuk <vasyaboychuk@gmail.com>
2018-01-04 10:15:31 +0200

    fix load path on production

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,6 @@ module Cats
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.autoload_paths += %W( lib/ )
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
